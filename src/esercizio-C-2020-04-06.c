3bbf002c44b8c5c9e4ac75dc776055c9f968f150
MattiaFurlan-ts <63098758+MattiaFurlan-ts@users.noreply.github.com>
2020-04-05 10:32:30 +0200

    Update esercizio-C-2020-04-06.c

--- src/esercizio-C-2020-04-06.c
+++ src/esercizio-C-2020-04-06.c
@@ -1,8 +1,34 @@
-/*
- * esercizio-C-2020-04-06.c
- *
- *  Created on: Apr 2, 2020
- *      Author: marco
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+void findBruteForce (int *a, int length)
+{
+    bool *visited = malloc (length * sizeof (bool));
+    for (int i = 0; i < length; i++)
+    {
+        int x = a[i];
+        if (!visited[i])
+        {
+            bool isDuplicate = false;
+            for (int j = i + 1; j < length; j++)
+            {
+                if (x == a[j])
+                {
+                    isDuplicate = true;
+                    visited[j] = true;
+                }
+            }
+            if (!isDuplicate)
+                printf ("Element appear only once in array - %d.\n", x);
+        }
+    }
+    free (visited);
+}
 
+int main ()
+{
+    int a[] = {1,5,6,2,1,6,4,3,2,5,3};
+    findBruteForce (a, sizeof (a) / sizeof (a[0]));
+    return 0;
+}
